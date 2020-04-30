fa1a574b9cacd2a6c50395c8c9804500c10ac1b1
Daniel Richard Condori Rodriguez <dan.rcr01@gmail.com>
2020-04-30 08:02:18 -0400

    This is a new arduino file handling analog inputs

--- AnalogInput.ino/AnalogInput.ino.ino
+++ AnalogInput.ino/AnalogInput.ino.ino
@@ -1,15 +1,29 @@
- int tiempo=0;
+ int tiempo;
  void setup() {
+  pinMode(13,INPUT);
   for(int i=2; i<10; i++){
       pinMode(i,OUTPUT);
     }
 }
 
-void loop() {    
-     for(int a=2;a<10;a++){
+void loop() {
+  iniciar:
+  for(int i=2; i<10; i++){
+      digitalWrite(i,0);
+    }
+  int button=digitalRead(13);
+  if(button){
+    delay(300);
+   while(1) {
+    for(int a=2;a<10;a++){
+      tiempo=analogRead(A0);
+      button=digitalRead(13);
+      if(button){delay(200); goto iniciar;}
       digitalWrite(a-1,LOW);
       digitalWrite(a,HIGH);
       delay(tiempo);
     }  
    digitalWrite(9,LOW);    
+  }
+}
 }
